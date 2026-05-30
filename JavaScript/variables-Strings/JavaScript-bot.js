ab1eb49eecd8eb7365a485529ba23c9f49ede57d
Duffy125 <cduffy125@gmail.com>
2026-05-30 09:05:41 -0700

    learing code for javaScript

---
+++ JavaScript/variables-Strings/JavaScript-bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "bot";
+let botLocation = "Oregon";
+let favoriteLanguage = "JavaScript";
+const botIntroduction = "My name is " + botName + " and I live on " + botLocation + ".";
+console.log(botIntroduction);
+const favoritProgramming= "My favorite programming language is " + favoriteLanguage + ".";
+console.log(favoritProgramming);
+let codingFact = favoritProgramming + "JavaScript is an introesting language to learn.";
+console.log(codingFact);
+
+codingFact = "Even after learning HTML and CSS, " + favoriteLanguage + " sames much more challaging to learn."
+console.log(codingFact);
+
+codingFact = "I'm hopping to learn" + favoriteLanguage + " in two monthes";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation +".")
+
