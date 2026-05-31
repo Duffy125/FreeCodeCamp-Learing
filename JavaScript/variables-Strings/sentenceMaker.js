ce8d2f8318edfb3077292ac8171f897ba04d8c7e
Duffy125 <cduffy125@gmail.com>
2026-05-31 08:55:58 -0700

    adding a new project to javaScript

---
+++ JavaScript/variables-Strings/sentenceMaker.js
@@ -0,0 +1,20 @@
+let adjective = "furry dog";
+let noun = "Bob";
+let verb = "runing";
+let place = "city";
+let adjective2 = "cat";
+let noun2 = "pizza";
+
+const firstStory = "Once upon a time, there was a(n) " + adjective +" " + noun + " who loved to eat " + noun2 +". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+console.log("First story: " + firstStory);
+
+adjective = "fat cat";
+noun = "Sam";
+verb = "walk";
+place = "barn";
+adjective2 = "puppy";
+noun2 = "fishing";
+
+const secondStory = "Once upon a time, there was a(n) " + adjective +" " + noun + " who loved to eat " + noun2 +". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+
+console.log("Second story: " + secondStory)
